c75d1adafbd70d04c2f43090dc4edba6be375453
nggasnik <steelangga@gmail.com>
2022-11-15 22:45:25 +0700

    latihan 1

---
+++ exercise.js
@@ -0,0 +1,11 @@
+var petDog = "Rex";
+var petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is:", petDog);
+console.log("My cat's name is:", petCat);
+
+var catSound = "meow";
+var dogSound = "woof";
+console.log(petDog,"says",dogSound);
+console.log(petCat,"now says",catSound);
